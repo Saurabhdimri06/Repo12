9afc1791527a43d7127024eefb492c5549f76054
dimrisaurabh <dimrisaurabh@gmail.com>
2018-03-27 14:55:03 +0530

    101 (modified)

--- hi.c
+++ hi.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	printf("HELLO");
-	printf("THIS IS IT THIS IS LIFE");
-	printf("LIFE AINT HOW HARD U HIT IT'S HOW HARD YOU CAN GET HIT AND KEEP MOVING FORWARD");
+	printf("HELLO");//commenting - comment1 
+	printf("THIS IS IT THIS IS LIFE");//commenting comment2
+	printf("LIFE AINT HOW HARD U HIT IT'S HOW HARD YOU CAN GET HIT AND KEEP MOVING FORWARD");//commenting comment3
 	return 0;
 }
