989be6519d3159f9bbf388491a18f6243de33c45
dimrisaurabh <dimrisaurabh@gmail.com>
2018-03-27 14:59:07 +0530

    101 (modified 2)

--- hi.c
+++ hi.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	printf("HELLO");
+	printf("HELLO WORLD Saurabh here");
 	printf("THIS IS IT THIS IS LIFE");
 	printf("LIFE AINT HOW HARD U HIT IT'S HOW HARD YOU CAN GET HIT AND KEEP MOVING FORWARD");
 	return 0;
