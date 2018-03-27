0ee9ed45419e92a211ac537f076ea1e0e2efb700
dimrisaurabh <dimrisaurabh@gmail.com>
2018-03-27 15:20:31 +0530

    102 (modified 3)

--- hi.c
+++ hi.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main()
 {
-	printf("HELLO");//commenting - comment1 
 	printf("THIS IS IT THIS IS LIFE");//commenting comment2
 	printf("LIFE AINT HOW HARD U HIT IT'S HOW HARD YOU CAN GET HIT AND KEEP MOVING FORWARD");//commenting comment3
 	return 0;
